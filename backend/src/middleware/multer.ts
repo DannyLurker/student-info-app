70073fc680e30187d0f78be88a2560a425aac8ff
DannyLurker <dannycode992@gmail.com>
2025-09-08 18:00:10 +0700

    feat: implement multer middleware for file upload with validation for .xls and .xlsx formats

---
+++ backend/src/middleware/multer.ts
@@ -0,0 +1,37 @@
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
+import { IRequest } from "../types/expressTypes.js";
+import AppError from "../utils/appError.js";
+
+const ALLOWED_EXTENSIONS = [".xls", ".xlsx"];
+const ALLOWED_MIME_TYPES = [
+  "application/vnd.ms-excel",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+];
+
+const storage = multer.memoryStorage();
+
+const customFileFilter = (
+  req: IRequest,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+
+  const isExtensionValid = ALLOWED_EXTENSIONS.includes(ext);
+  const isMimeValid = ALLOWED_MIME_TYPES.includes(file.mimetype);
+
+  if (isExtensionValid && isMimeValid) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Invalid file type. Only .xls or .xlsx", 400));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: customFileFilter,
+});
+
+export default upload;
